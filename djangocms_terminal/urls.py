43ea9f07f0c25d395e3ca8bb34e326639e74fe5c
marco federighi <federighi.marco@gmail.com>
2015-10-24 11:15:06 +0200

    Check which urls config use based on django vers.

--- djangocms_terminal/urls.py
+++ djangocms_terminal/urls.py
@@ -1,13 +1,23 @@
-from django.conf.urls import url
+import django
+from django.conf.urls import url, patterns
 
 from . import views
 from django.views.generic import TemplateView
 
 
-urlpatterns = [
-    url(r'^apps_list/$', views.installed_apps),
-    url(r'^models_list/$', views.get_models),
-    url(r'^model_fields/$', views.model_fields),
-    url(r'^model_instance/$', views.model_instance),
-    url(r'^$', TemplateView.as_view(template_name='djangocms_terminal/terminal.html'))
-]
+if django.get_version() < '1.8':
+    urlpatterns = patterns('',
+        url(r'^apps_list/$', views.installed_apps),
+        url(r'^models_list/$', views.get_models),
+        url(r'^model_fields/$', views.model_fields),
+        url(r'^model_instance/$', views.model_instance),
+        url(r'^$', TemplateView.as_view(template_name='djangocms_terminal/terminal.html'))
+    )
+else:
+    urlpatterns = [
+        url(r'^apps_list/$', views.installed_apps),
+        url(r'^models_list/$', views.get_models),
+        url(r'^model_fields/$', views.model_fields),
+        url(r'^model_instance/$', views.model_instance),
+        url(r'^$', TemplateView.as_view(template_name='djangocms_terminal/terminal.html'))
+    ]
