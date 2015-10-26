5b831050d15cc754f2f7e2b0e0bb97fc642118fa
marco federighi <federighi.marco@gmail.com>
2015-10-26 08:34:46 +0100

    Add initial tests config

---
+++ test_settings.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+HELPER_SETTINGS = {
+}
+
+
+def run():
+    from djangocms_helper import runner
+    runner.cms('djangocms_terminal')
+
+if __name__ == '__main__':
+    run()
