94d6537fccb3b4b24939710b498857f7aa10b3d6
marco federighi <federighi.marco@gmail.com>
2015-10-26 20:30:27 +0100

    Fix django version check

--- djangocms_terminal/utils.py
+++ djangocms_terminal/utils.py
@@ -3,9 +3,13 @@ import os
 
 import django
 if django.get_version() < '1.8':
-    from django.db.models import loading
+    from django.db.models import loading, get_app, get_model
+    from django.db.models import get_models
 else:
     from django.apps import apps
+    from django.apps.apps import get_model
+    from django.apps.apps import get_models
+
 
 
 def get_module_name(complete_name):
@@ -20,7 +24,10 @@ def get_installed_apps():
         return [app.name for app in apps.get_app_configs()]
 
 def get_app_models(app_label):
-    return [model.__name__ for model in apps.get_app_config(app_label).get_models()]
+    if django.get_version() < '1.8':
+        return get_models(get_app(app_label))
+    else:
+        return [model.__name__ for model in apps.get_app_config(app_label).get_models()]
 
 def get_app_model(app_label, model_name):
-    return apps.get_model(app_label=app_label, model_name=model_name)
+    return get_model(app_label=app_label, model_name=model_name)
